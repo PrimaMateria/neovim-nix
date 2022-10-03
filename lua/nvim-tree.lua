eb6d045566b85d6db44339ed364186d993f30b59
matus.benko <matus.benko@gmail.com>
2022-10-03 14:04:15 +0200

    update nvim tree

--- lua/nvim-tree.lua
+++ lua/nvim-tree.lua
@@ -4,6 +4,9 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup({
 	view = {
 		adaptive_size = true,
+		hide_root_folder = true,
+		number = true,
+		relativenumber = true,
 	},
 	git = {
 		ignore = false,
