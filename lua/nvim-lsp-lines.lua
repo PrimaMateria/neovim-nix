05ca69bacb8afdd0c78912d08ee4ce8f5f4d1260
matus.benko <matus.benko@gmail.com>
2022-10-05 13:10:03 +0200

    Keymap to toggle virtual lines

--- lua/nvim-lsp-lines.lua
+++ lua/nvim-lsp-lines.lua
@@ -2,4 +2,8 @@ require("lsp_lines").setup()
 
 vim.diagnostic.config({
 	virtual_text = false,
+	virtual_lines = false,
 })
+
+local bufopts = { noremap = true, silent = true }
+vim.keymap.set("n", ",e", require("lsp_lines").toggle, bufopts)
