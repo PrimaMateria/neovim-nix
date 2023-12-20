e03ffedd3d79efaa9486eb253cd2e00bec55b9f4
matus.benko <matus.benko@gmail.com>
2023-12-20 15:01:00 +0100

    Updates for noice

--- config/lua/nvim-lualine.lua
+++ config/lua/nvim-lualine.lua
@@ -7,7 +7,28 @@ require("lualine").setup({
 		},
 	},
 	sections = {
-		lualine_c = { "require'lsp-status'.status()" },
+		lualine_c = {},
+		lualine_x = {
+			{
+				require("noice").api.status.message.get_hl,
+				cond = require("noice").api.status.message.has,
+			},
+			{
+				require("noice").api.status.command.get,
+				cond = require("noice").api.status.command.has,
+				color = { fg = "#ff9e64" },
+			},
+			{
+				require("noice").api.status.mode.get,
+				cond = require("noice").api.status.mode.has,
+				color = { fg = "#ff9e64" },
+			},
+			{
+				require("noice").api.status.search.get,
+				cond = require("noice").api.status.search.has,
+				color = { fg = "#ff9e64" },
+			},
+		},
 	},
 
 	winbar = {
