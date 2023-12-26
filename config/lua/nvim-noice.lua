2356adf478b61568dd6ab2f6c6712ce312b4aa84
matus.benko <matus.benko@gmail.com>
2023-12-26 14:38:18 +0100

    less noisy noice

--- config/lua/nvim-noice.lua
+++ config/lua/nvim-noice.lua
@@ -18,8 +18,8 @@ require("noice").setup({
 	messages = {
 		enabled = true, -- enables the Noice messages UI
 		view = "mini", -- default view for messages
-		view_error = "notify", -- view for errors
-		view_warn = "notify", -- view for warnings
+		view_error = "mini", -- view for errors
+		view_warn = "mini", -- view for warnings
 		view_history = "messages", -- view for :messages
 		view_search = false, -- view for search count messages. Set to `false` to disable
 	},
