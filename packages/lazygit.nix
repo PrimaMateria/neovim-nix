d7b1cccd9b50c247f7ed8874df2c12c18a75c300
matus.benko <matus.benko@gmail.com>
2024-04-16 16:10:38 +0200

    lazygit commitPrefixes config

--- packages/lazygit.nix
+++ packages/lazygit.nix
@@ -1,26 +1,38 @@
-{ prev, ... }:
-let 
-  lazygitConfig = prev.writeTextFile {
-    name = "lazygitConfig.yaml";
-    text = ''
-      os:
-        edit: '$NVIM_PRIMAMATERIA --server "$NVIM" --remote-tab {{filename}}'
-        editAtLine: '$NVIM_PRIMAMATERIA --server "$NVIM" --remote-tab {{filename}}; [ -z "$NVIM" ] || $NVIM_PRIMAMATERIA --server "$NVIM" --remote-send ":{{line}}<CR>"'
-        editAtLineAndWait: '$NVIM_PRIMAMATERIA +{{line}} {{filename}}'
-        openDirInEditor: '$NVIM_PRIMAMATERIA --server "$NVIM" --remote-tab {{dir}}'
-        suspend: false
-
-      git:
-        commitPrefixes:
-          # TODO: add real projects' names or create a generator
-          experiment-lazygitConfig:
-            pattern: "^\\w+\\/(\\w+-\\w+).*"
-            replace: '$1:'
-    '';
+{prev, ...}: let
+  lazygitConfig = (prev.formats.yaml {}).generate "lazygit-config.yaml" {
+    os = {
+      edit = ''$NVIM_PRIMAMATERIA --server "$NVIM" --remote-tab {{filename}}'';
+      editAtLine = ''$NVIM_PRIMAMATERIA --server "$NVIM" --remote-tab {{filename}}; [ -z "$NVIM" ] || $NVIM_PRIMAMATERIA --server "$NVIM" --remote-send ":{{line}}<CR>"'';
+      editAtLineAndWait = ''$NVIM_PRIMAMATERIA +{{line}} {{filename}}'';
+      openDirInEditor = ''$NVIM_PRIMAMATERIA --server "$NVIM" --remote-tab {{dir}}'';
+      suspend = false;
+    };
+    git.commitPrefixes = builtins.listToAttrs (
+      map (projectName: {
+        name =
+          projectName;
+        value = {
+          pattern = "^\\w+\\/(\\w+-\\w+).*";
+          replace = "$1: ";
+        };
+      })
+      [
+        "finapi-cms"
+        "finapi-customer-dashboard-ui"
+        "finapi-design-system"
+        "finapi-hostpages"
+        "finapi-js-loader"
+        "finapi-js-static-resources"
+        "finapi-widget-library"
+        "web-form"
+        "web-form-loader"
+      ]
+    );
   };
-in prev.writeShellApplication {
-  name = "lazygit";
-  text = ''
-    ${prev.lazygit}/bin/lazygit --use-config-file ${lazygitConfig} "$@"
-  '';
-}
+in
+  prev.writeShellApplication {
+    name = "lazygit";
+    text = ''
+      ${prev.lazygit}/bin/lazygit --use-config-file ${lazygitConfig} "$@"
+    '';
+  }
