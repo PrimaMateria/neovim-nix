d0ce585292414a9322b36d316a1ac6f37b93b663
matus.benko <matus.benko@gmail.com>
2024-08-26 10:58:06 +0200

    init keymap scheme

--- config/lua/nvim-avante.lua
+++ config/lua/nvim-avante.lua
@@ -1,1 +1,28 @@
-require("avante").setup({})
+require("avante").setup({
+	mappings = {
+		ask = ",sd",
+		edit = ",sf",
+		refresh = nil,
+		--- @class AvanteConflictMappings
+		diff = {
+			ours = "co",
+			theirs = "ct",
+			none = "c0",
+			both = "cb",
+			next = "]x",
+			prev = "[x",
+		},
+		jump = {
+			next = "]]",
+			prev = "[[",
+		},
+		submit = {
+			normal = "<CR>",
+			insert = "<C-s>",
+		},
+		toggle = {
+			debug = "<leader>ad",
+			hint = "<leader>ah",
+		},
+	},
+})
--- config/lua/nvim-openai.lua
+++ config/lua/nvim-openai.lua
@@ -2,5 +2,5 @@ require("chatgpt").setup({
 	welcome_message = "ChatGPT",
 })
 
-vim.api.nvim_set_keymap("n", "<leader>aa", "<cmd>ChatGPT<cr>", { noremap = true })
-vim.api.nvim_set_keymap("v", ",ag", "<cmd>ChatGPTRun grammar_correction<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", ",sa", "<cmd>ChatGPT<cr>", { noremap = true })
+vim.api.nvim_set_keymap("v", ",sf", "<cmd>ChatGPTRun grammar_correction<cr>", { noremap = true })
