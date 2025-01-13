f0e369da987d758bf2b2b8454e6216bb59f93dea
matus.benko <matus.benko@gmail.com>
2025-01-13 11:31:22 +0100

    blink border

--- src/packages/neovim/base/__config/lua/nvim-blink.lua
+++ src/packages/neovim/base/__config/lua/nvim-blink.lua
@@ -27,12 +27,14 @@ blink.setup({
 	},
 
 	completion = {
+		menu = { border = "single" },
 		documentation = {
 			auto_show = true,
 			auto_show_delay_ms = 500,
+			window = { border = "single" },
 		},
 		ghost_text = { enabled = true },
 	},
 
-	signature = { enabled = true },
+	signature = { enabled = true, window = { border = "single" } },
 })
