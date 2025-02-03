de33dcc9636d0976da68ce7bdaa4097d2ed29d8d
matus.benko <matus.benko@gmail.com>
2025-02-03 17:13:26 +0100

    typescript unified imports command

--- src/packages/neovim/web/__config/lua/nvim-typescript-tools.lua
+++ src/packages/neovim/web/__config/lua/nvim-typescript-tools.lua
@@ -1,9 +1,14 @@
 require("typescript-tools").setup({})
 
-vim.api.nvim_set_keymap("n", ",ai", "<cmd>TSToolsOrganizeImports<cr>", { noremap = true, desc = "Organize imports" })
 vim.api.nvim_set_keymap(
 	"n",
-	",aI",
-	"<cmd>TSToolsAddMissingImports<cr>",
-	{ noremap = true, desc = "Add missing imports" }
+	",ai",
+	"<cmd>TSToolsOrganizeImports<cr><cmd>TSToolsAddMissingImports<cr>",
+	{ noremap = true, desc = "Organize imports" }
 )
+-- vim.api.nvim_set_keymap(
+-- 	"n",
+-- 	",aI",
+-- 	"<cmd>TSToolsAddMissingImports<cr>",
+-- 	{ noremap = true, desc = "Add missing imports" }
+-- )
