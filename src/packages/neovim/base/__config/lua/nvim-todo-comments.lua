6977ef7a5488bc4dc39b385bb93e1bb7d98c560d
matus.benko <matus.benko@gmail.com>
2025-02-26 17:20:17 +0100

    todo list keymap

--- src/packages/neovim/base/__config/lua/nvim-todo-comments.lua
+++ src/packages/neovim/base/__config/lua/nvim-todo-comments.lua
@@ -1,1 +1,10 @@
-require("todo-comments").setup({})
+require("todo-comments").setup({
+	signs = false,
+	highlight = {
+		before = "bg",
+		keyword = "bg",
+		after = "bg",
+	},
+})
+
+vim.keymap.set("n", ",t", "<cmd>TodoTelescope<CR>", { silent = true, desc = "List TODOs" })
