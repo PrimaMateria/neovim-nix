34c41b67cb224b39a2904e69ce9cc9677698fff6
matus.benko <matus.benko@gmail.com>
2025-08-26 16:42:43 +0200

    oil config update

--- src/packages/neovim/base/__config/lua/nvim-oil.lua
+++ src/packages/neovim/base/__config/lua/nvim-oil.lua
@@ -1,4 +1,12 @@
-require("oil").setup()
+require("oil").setup({
+	buf_options = {
+		buflisted = true,
+		-- bufhidden = "",
+	},
+	cleanup_delay_ms = false,
+	skip_confirm_for_simple_edits = true,
+})
+
 local wk = require("which-key")
 
 vim.keymap.set("n", ",o", "<cmd>Oil<CR>", { silent = true })
