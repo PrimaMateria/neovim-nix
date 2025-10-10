28ed75be41df2b9eb6ad80f2440e056b8e061a04
matus.benko <matus.benko@gmail.com>
2025-10-10 13:58:26 +0200

    lualine update

--- src/packages/neovim/light/__config/lua/nvim-lualine.lua
+++ src/packages/neovim/light/__config/lua/nvim-lualine.lua
@@ -7,35 +7,13 @@ require("lualine").setup({
 		},
 	},
 	sections = {
-		lualine_c = { { "filename", path = 2 } },
-		lualine_x = {
-			{
-				require("noice").api.status.message.get_hl,
-				cond = require("noice").api.status.message.has,
-			},
-			{
-				require("noice").api.status.command.get,
-				cond = require("noice").api.status.command.has,
-				color = { fg = "#ff9e64" },
-			},
-			{
-				require("noice").api.status.mode.get,
-				cond = require("noice").api.status.mode.has,
-				color = { fg = "#ff9e64" },
-			},
-			{
-				require("noice").api.status.search.get,
-				cond = require("noice").api.status.search.has,
-				color = { fg = "#ff9e64" },
-			},
-		},
-	},
+		lualine_a = { "mode" },
+		lualine_b = { { "filename", path = 2 } },
+		lualine_c = {},
 
-	winbar = {
-		lualine_c = { "filename" },
-	},
-	inactive_winbar = {
-		lualine_c = { "filename" },
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
 	},
 	tabline = {
 		lualine_a = { {
