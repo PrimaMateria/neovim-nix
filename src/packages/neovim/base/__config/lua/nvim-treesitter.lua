7a1495734f81fd20b47661fdfd8dfb4464660b63
matus.benko <matus.benko@gmail.com>
2026-02-09 17:22:17 +0100

    treesitter fold fix

--- src/packages/neovim/base/__config/lua/nvim-treesitter.lua
+++ src/packages/neovim/base/__config/lua/nvim-treesitter.lua
@@ -2,13 +2,30 @@ require("nvim-treesitter").setup({})
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	callback = function()
-		-- syntax highlighting, provided by Neovim
-		vim.treesitter.start()
-		-- folds, provided by Neovim
-		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-		vim.wo.foldmethod = "expr"
-		-- indentation, provided by nvim-treesitter
-		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		-- skip special buffers
+		if vim.bo.buftype ~= "" or vim.bo.filetype == "" then
+			return
+		end
+
+		-- try to start tree-sitter; not all filetypes have parsers (e.g. fugitiveblame)
+		local ok = pcall(vim.treesitter.start, 0)
+		if ok then
+			-- folds, provided by Neovim tree-sitter
+			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			vim.wo.foldmethod = "expr"
+			-- start with folds open
+			vim.wo.foldlevel = 99
+			-- indentation, provided by nvim-treesitter
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		else
+			-- fallback for buffers without a parser
+			vim.wo.foldexpr = ""
+			vim.wo.foldmethod = "manual"
+			-- optional: clear indentexpr if previously set
+			if vim.bo.indentexpr == "v:lua.require'nvim-treesitter'.indentexpr()" then
+				vim.bo.indentexpr = ""
+			end
+		end
 	end,
 })
 
